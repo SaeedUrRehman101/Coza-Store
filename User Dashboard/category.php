a82fc5b6617c600bc905193623de7e5d41819276
saeedurrehmnan101 <saeedtutorial786@gmail.com>
2024-09-13 11:02:31 +0500

    dashboard

---
+++ User Dashboard/category.php
@@ -0,0 +1,48 @@
+<?php
+include('components/header.php')
+?>
+
+<!-- Blank Start -->
+<div class="container-fluid pt-4 px-4">
+                <div class="row bg-light rounded mx-0">
+                    <div class="col-md-12">
+                        <h3 class="px-3 py-4">All Categories</h3>
+                        <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">First Name</th>
+                                        <th scope="col">Last Name</th>
+                                        <th scope="col">Email</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>John</td>
+                                        <td>Doe</td>
+                                        <td>jhon@email.com</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>mark@email.com</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>jacob@email.com</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                    </div>
+                </div>
+            </div>
+            <!-- Blank End -->
+
+
+<?php
+include('components/footer.php')
+?>
